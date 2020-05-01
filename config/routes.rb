21ff6da260e4ee16cb7b9704ae49779aa946db63
Nico Lauria <nflauria@outlook.com>
2020-04-30 20:38:44 -0700

    add static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  match "*path", to: "static_pages#index", via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
